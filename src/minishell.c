df2a4b3e2f1693ebc6f2d3b7616c01287bffac94
ryoshio- <ryoshio-@student.42sp.org.br>
2022-08-20 01:08:30 +0200

    finishing day

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 18:03:23 by lucasmar          #+#    #+#             */
-/*   Updated: 2022/08/19 23:38:57 by ryoshio-         ###   ########.fr       */
+/*   Updated: 2022/08/20 00:37:23 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int main(int argc, char **argv, char **envp)
 		{
 			ft_cmd(&ms);
 			ft_printf("%s\n ", ms.cmd);
+			add_history(ms.cmd);
 			free(ms.cmd);
 		}
 	}
@@ -42,7 +43,9 @@ void ft_cmd(t_ms * ms)
 
 	if (ft_strncmp(ms->cmd, "exit", 4) == 0)
 	{
+		rl_clear_history();
 		free(ms->cmd);
+		
 		exit(EXIT_SUCCESS);
 		
 	}
