9d5c5bf6e983a3579a1e58f88ff50c2ad50702e9
Lucas <apontes19@gmail.com>
2022-09-02 17:29:33 +0200

    split concluida com a norminette

--- src/ft_split_ms.c
+++ src/ft_split_ms.c
@@ -6,77 +6,91 @@
 /*   By: lucasmar < lucasmar@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 15:07:58 by lucasmar          #+#    #+#             */
-/*   Updated: 2022/09/02 15:06:18 by lucasmar         ###   ########.fr       */
+/*   Updated: 2022/09/02 17:28:40 by lucasmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int		set_size(const char *str, char c);
-static char		*duplicate(const char *str, int start, int end);
-static int		next_quote(const char *s, char **result);
-
-char	**ft_split_ms(char const *s, char c)
+typedef struct _var
 {
-	size_t	counter;
+	size_t	i;
 	size_t	p;
 	int		index;
 	char	**result;
+	int		quote;
+}t_var;
 
-	result = malloc((set_size(s, c) + 1) * sizeof(char *));
-	if (!s || !result)
-		return (0);
-	counter = 0;
-	p = 0;
-	index = -1;
-	while (counter <= ft_strlen(s))
+static int		set_size(const char *str, char c);
+static char		*duplicate(const char *str, int start, int end);
+static int		next_quote(const char *s, char **result);
+static int		init_var(t_var *var, char const *s, char c);
+
+char	**ft_split_ms(char const *s, char c)
+{
+	t_var	var;
+
+	init_var(&var, s, c);
+	while (var.i <= ft_strlen(s))
 	{
-		printf("Valor fora da \' P:%zu\n",p);
-		if ((s[counter] == '\'' || s[counter] == '\"') && c == ' ')
+		if (s[var.i] == '\'' || s[var.i] == '\"')
 		{
-			counter += next_quote(&s[counter], &result[p++]);
-			printf("Valor dentro da \' P:%zu\n",p);
+			var.quote++;
+			if (c == ' ')
+				var.i += next_quote(&s[var.i], &var.result[var.p++]);
+			else if (var.quote > 1)
+				var.quote = 0;
 		}
-		if (s[counter] != c && index < 0)
-			index = counter;
-		else if (((s[counter] == c &&)|| counter == ft_strlen(s)) && index >= 0)
+		if (s[var.i] != c && var.index < 0)
+			var.index = var.i;
+		else if (((s[var.i] == c && var.quote == 0) || \
+			var.i == ft_strlen(s)) && var.index >= 0)
 		{
-			result[p++] = duplicate(s, index, counter);
-			index = -1;
+			var.result[var.p++] = duplicate(s, var.index, var.i);
+			var.index = -1;
 		}
-		counter++;
+		var.i++;
 	}
-	result[p] = 0;
-	return (result);
+	var.result[var.p] = 0;
+	return (var.result);
+}
+
+static int	init_var(t_var *var, char const *s, char c)
+{
+	ft_memset(var, 0, sizeof var);
+	var->i = -0;
+	var->p = 0;
+	var->index = -1;
+	var->quote = 0;
+	var->result = malloc((set_size(s, c) + 1) * sizeof(char *));
+	if (!s || !var->result)
+		return (0);
+	return (1);
 }
 
 static int	set_size(const char *str, char c)
 {
 	int	size;
 	int	apart;
-	char	type;
+	int	quote;
 
+	quote = 0;
 	size = 0;
 	apart = 0;
 	while (*str)
 	{
 		if (*str == '\'' || *str == '\"')
 		{
-			str++;
-			type = '\'';
-			if (*str == '\"')
-				type = '\"';
-			while(*str != type)
-				str++;
-			str++;
-			apart = 1;
+			quote++;
+			if (quote > 1)
+				quote = 0 ;
 		}
 		if (*str != c && apart == 0)
 		{
 			apart = 1;
 			size++;
 		}
-		else if (*str == c)
+		else if (*str == c && quote == 0)
 			apart = 0;
 		str++;
 	}
@@ -86,33 +100,33 @@ static int	set_size(const char *str, char c)
 static char	*duplicate(const char *str, int start, int end)
 {
 	char	*duplicate;
-	int		counter;
+	int		i;
 
-	counter = 0;
+	i = 0;
 	duplicate = malloc((end - start + 1) * sizeof(char));
 	while (start < end)
 	{
-		duplicate[counter] = str[start];
-		counter++;
+		duplicate[i] = str[start];
+		i++;
 		start++;
 	}
-	duplicate[counter] = '\0';
+	duplicate[i] = '\0';
 	return (duplicate);
 }
 
 static int	next_quote(const char *s, char **result)
 {
-	int	counter;
+	int		i;
 	char	type;
 
-	counter = 1;
+	i = 1;
 	type = '\'';
-	if (s[counter] == '\"')
+	if (s[i] == '\"')
 		type = '\"';
-	while(s[counter] != type)
+	while (s[i] != type)
 	{
-		counter++;
+		i++;
 	}
-	*result = duplicate(s, 0, (counter+1));
-	return (counter+1);
+	*result = duplicate(s, 0, (i + 1));
+	return (i + 1);
 }
