95384fc98892fb3de4ae69fb66a3c0245758596d
Lucas <apontes19@gmail.com>
2022-09-26 20:40:17 +0200

    new cd

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lucasmar < lucasmar@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:36:27 by lucasmar          #+#    #+#             */
-/*   Updated: 2022/09/26 01:06:51 by lucasmar         ###   ########.fr       */
+/*   Updated: 2022/09/26 20:29:21 by lucasmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,4 +65,7 @@ void	ft_cd(t_ms *ms, t_cmd *cmd);
 void	ft_cd_error(t_ms *ms, t_cmd *cmd);
 int		ft_valid_dir(char *path);
 //void	ft_copy_envp(char **envp);
+void	ft_minus_cd(t_ms *ms);
+void	ft_change_cd(t_ms *ms, char *change);
+
 #endif
--- src/cd.c
+++ src/cd.c
@@ -6,7 +6,7 @@
 /*   By: lucasmar < lucasmar@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 19:50:11 by lucasmar          #+#    #+#             */
-/*   Updated: 2022/09/26 20:18:38 by lucasmar         ###   ########.fr       */
+/*   Updated: 2022/09/26 20:39:19 by lucasmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	ft_cd(t_ms *ms, t_cmd *cmd)
 {
-	char	*temp;
-
 	if ( ms->n_pipe == 1)
 	{
 		if (g_ms.cd == 0)
@@ -24,31 +22,12 @@ void	ft_cd(t_ms *ms, t_cmd *cmd)
 			ft_error(16, ms);
 		else if (cmd[ms->p].arg_cmd[1] == NULL ||
 			ft_strncmp(cmd[ms->p].arg_cmd[1], "~", 1) == 0)
-		{
-			getcwd(ms->oldpwd, 1024);
-			chdir(getenv("HOME"));
-			g_ms.cd++;
-		}
+			ft_change_cd(ms, "HOME");
 		else if (ft_strncmp(cmd[ms->p].arg_cmd[1], "-", 1) == 0)
-		{
-			if (g_ms.cd == 0)
-				ft_error(17, ms);
-			else
-			{
-				temp = ft_strdup(ms->oldpwd);
-				getcwd(ms->oldpwd, 1024);
-				ft_printf("%s\n", temp);
-				chdir(temp);
-				free(temp);
-			}
-		}
+			ft_minus_cd(ms);
 		else if (access(cmd[ms->p].arg_cmd[1], F_OK) == 0
 			&& ft_valid_dir(cmd[ms->p].arg_cmd[1]) == 1)
-		{
-			getcwd(ms->oldpwd, 1024);
-			chdir (cmd[ms->p].arg_cmd[1]);
-			g_ms.cd++;
-		}
+			ft_change_cd(ms, cmd[ms->p].arg_cmd[1]);
 		else
 			ft_cd_error(ms, cmd);
 	}
@@ -62,4 +41,30 @@ int	ft_valid_dir(char *path)
 	if (stat(path, &statbuf) != 0)
 		return (0);
 	return (S_ISDIR(statbuf.st_mode));
+}
+
+void	ft_change_cd(t_ms *ms, char *change)
+{
+	getcwd(ms->oldpwd, 1024);
+	if (ft_strncmp(change, "HOME", 4) == 0)
+		chdir(getenv(change));
+	else
+		chdir(change);
+	g_ms.cd++;
+}
+
+void	ft_minus_cd(t_ms *ms)
+{
+	char	*temp;
+
+	if (g_ms.cd == 0)
+		ft_error(17, ms);
+	else
+	{
+		temp = ft_strdup(ms->oldpwd);
+		getcwd(ms->oldpwd, 1024);
+		ft_printf("%s\n", temp);
+		chdir(temp);
+		free(temp);
+	}
 }
